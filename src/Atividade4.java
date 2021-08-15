c4839fdeef5a652eb1d11e3455266cf8fad40d65
brunobutka <brunobutka2010@hotmail.com>
2021-08-15 13:20:39 -0300

    Atividade 4 inicio

--- src/Atividade4.java
+++ src/Atividade4.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDate;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,9 @@ public class Atividade4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String valorCompra = null, totalParcelas = null, exibicaoDatas = null;
+        LocalDate dataAtual = LocalDate.now();
+        
     }
     
 }
